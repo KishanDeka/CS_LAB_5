befa3c6add7138689447d12a083f5d6fbb242cc5
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-10-13 20:08:17 +0530

    Update lab_question_2.cpp

--- lab_question_2.cpp
+++ lab_question_2.cpp
@@ -44,8 +44,42 @@ void median(int A[], int n)
                  q=n/2;
                  cout<<"median = "<<A[p]<<" and " <<A[q]<<endl;}
     else { p=(n-1)/2;
-           cout<<"median = "<<A[p]<<endl;}
+           cout<<"Median = "<<A[p]<<endl;}
 }
+
+void mode(int A[], int n)
+{
+    int e;
+    for(int i=0;i<n;i++)
+       { for (int j=i;j<n;j++)
+             { if (A[j]>A[j+1])
+                 { e=A[j];
+                   A[j]=A[j+1];
+                   A[j+1]=e;
+                 }
+             }
+       }
+     	int a=0, b=0;
+	int K=0;
+	for (int i=0;i< n-1;i++)
+    {
+		if (A[i] == A[i+1])
+        {
+			a++;
+		}
+		else
+		{
+			if (a>b)
+            {
+				b=a;
+				K=i;
+			}
+			a=0;
+		}
+	}
+	cout<<"The element having highest frequency in the array : "<<A[K];
+}
+
 int main()
 {    int n;
     cout << "enter the size of array : ";
@@ -57,9 +91,10 @@ int main()
           cout<< "Element "<<p+1<<" : ";
           cin >> A[p];
         }
-    cout <<"max = "<< Max(A, n)<<endl;
-    cout <<"min = "<< Min(A,n)<<endl;
+    cout <<"largest element of the array = "<< Max(A, n)<<endl;
+    cout <<"smallest element of the array = "<< Min(A,n)<<endl;
     cout<<"mean = "<<mean(A,n)<<endl;
     median(A,n);
+    mode(A,n);
     return 0;
 }
